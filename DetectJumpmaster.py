25d3a982fdb12303760b6e4647cb5bbe8e2f4aa7
Julius <julius.ostertag@gmail.com>
2021-09-16 19:42:12 +0200

    Änderungen!

--- DetectJumpmaster.py
+++ DetectJumpmaster.py
@@ -20,8 +20,16 @@ def screen_record_efficient(sc):
     res = cv2.matchTemplate(img_gray, jumpmasterIcon, cv2.TM_CCOEFF_NORMED)
     _, max_val, _, _ = cv2.minMaxLoc(res)
     print(max_val)
-    
+    if max_val >= 0.8: 
+        sendNotification()
+   
     s.enter(delayInSeconds, 1, screen_record_efficient, (sc,))
 
 s.enter(delayInSeconds, 1, screen_record_efficient, (s,))
 s.run()
+
+def sendNotification():
+    playSound()
+    showPopup()
+
+def
