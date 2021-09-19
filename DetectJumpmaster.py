7e718695c8127f2a3f908a79c3b171f4092ef48c
Lucas <lucaskeller98@gmail.com>
2021-09-19 11:50:29 +0200

    take screenshot of apex even if not in foreground

--- DetectJumpmaster.py
+++ DetectJumpmaster.py
@@ -1,8 +1,10 @@
+import win32gui
+import win32ui
+import win32con
 import json
 import requests
 import time
 import sched
-import mss
 import numpy
 import cv2
 import os
@@ -64,63 +66,120 @@ def sendDiscordDM(message):
     requests.post(msgURL, headers=msgHeaders, data=msgJSON)
 
 
+def lookForApex():
+    print("Apex not found. Waiting for Apex to start...")
+    while 'Apex not found':
+        try:
+            hwnd = win32gui.FindWindow(None, 'Apex Legends')
+            _, top, _, bot = win32gui.GetWindowRect(hwnd)
+            print('Apex detected!')
+            break
+        except:
+            time.sleep(1)
+    return hwnd, top, bot
+
+
+def takeScreenshot(isJumpmaster):
+    # https://answers.opencv.org/question/229026/help-with-optimization-opencv-python/
+
+    try:
+        hwnd = win32gui.FindWindow(None, 'Apex Legends')
+        _, top, _, bot = win32gui.GetWindowRect(hwnd)
+    except:
+        hwnd, top, bot = lookForApex()
+
+    height = bot - top
+    is1080p = True if 1070 <= height <= 1090 else False
+
+    filePath, imgPosition = getIconAndPosition(isJumpmaster, is1080p)
+
+    imgLeft = imgPosition['left']
+    imgTop = imgPosition['top']
+    imgWidth = imgPosition['width']
+    imgHeight = imgPosition['height']
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+
+    saveBitMap.CreateCompatibleBitmap(mfcDC, imgWidth, imgHeight)
+    saveDC.SelectObject(saveBitMap)
+
+    saveDC.BitBlt((0, 0), (imgWidth, imgHeight), mfcDC,
+                  (imgLeft, imgTop), win32con.SRCCOPY)
+    signedIntsArray = saveBitMap.GetBitmapBits(True)
+    img = numpy.frombuffer(signedIntsArray, dtype='uint8')
+    img.shape = (imgHeight, imgWidth, 4)
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    img_gray = cv2.cvtColor(numpy.array(img), cv2.COLOR_BGR2GRAY)
+    return img_gray, filePath
+
+
 def image_comp(isJumpmaster):
-    sct = mss.mss()
+    img_gray, filePath = takeScreenshot(isJumpmaster)
 
-    monitorHeight = sct.monitors[0]['height']
-    is1080p = True if 1070<= monitorHeight<= 1090 else False # bei mir ist's 1081 bei 1080p?
-
-    filePath, iconPosition = getIconAndPosition(isJumpmaster, is1080p)
     imgToCompare = cv2.imread(filePath, 0)
 
-    img = numpy.asarray(sct.grab(iconPosition))
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     res = cv2.matchTemplate(img_gray, imgToCompare, cv2.TM_CCOEFF_NORMED)
     _, max_val, _, _ = cv2.minMaxLoc(res)
 
-    # cv2.imshow("Image", img)
+    # cv2.imshow("Image", img_gray)
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     print('Fenster wird geschlossen')
     return max_val
 
+
 def getIconAndPosition(isJumpmaster, is1080p):
     jumpmasterFilePath1440 = resource_path('./images/jumpIcon1440.png')
     jumpmasterFilePath1080 = resource_path('./images/jumpIcon1080.png')
-    jumpmasterIconPosition1440 = {'left': 1100, 'top': 1076, 'width': 80, 'height': 80}
-    jumpmasterIconPosition1080 = {'left': 835, 'top': 817, 'width': 70, 'height': 70}
+    jumpmasterIconPosition1440 = {'left': 1100, 'top': 1076,
+                                  'width': 80, 'height': 80}
+    jumpmasterIconPosition1080 = {'left': 835, 'top': 817,
+                                  'width': 70, 'height': 70}
 
     charFilePath1440 = resource_path('./images/bloodIcon1440.png')
     charFilePath1080 = resource_path('./images/bloodIcon1080.png')
-    charIconPosition1440 = {'left': 1509, 'top': 435, 'width': 70, 'height': 70}
-    charIconPosition1080 = {'left': 1123, 'top': 328, 'width': 70, 'height': 70}
+    charIconPosition1440 = {'left': 1509, 'top': 435,
+                            'width': 70, 'height': 70}
+    charIconPosition1080 = {'left': 1123, 'top': 328,
+                            'width': 70, 'height': 70}
+
     if isJumpmaster and is1080p:
-        return  jumpmasterFilePath1080, jumpmasterIconPosition1080
+        return jumpmasterFilePath1080, jumpmasterIconPosition1080
     elif isJumpmaster and not is1080p:
         return jumpmasterFilePath1440, jumpmasterIconPosition1440
     elif not isJumpmaster and is1080p:
-        print('here')
         return charFilePath1080, charIconPosition1080
     else:
         return charFilePath1440, charIconPosition1440
+
 
 def playRandomSound():
     randomIndex = randrange(8)  # 0-7
     soundToPlay = resource_path(f'sounds/voice{randomIndex+1}.mp3')
     playsound(soundToPlay)
 
+
 def detect_jumpmaster(sc):
     global currentIteration
     maxIteration = 120
-    
+
     max_val = image_comp(isJumpmaster=True)
 
     print(
-        f"jumpmaster detected: {'true' if max_val >= 0.8 else 'false'} - {currentIteration}/{maxIteration}")
+        f"Jumpmaster detected: {'true' if max_val >= 0.8 else 'false'} - {currentIteration}/{maxIteration}")
 
     event = s.enter(1, 1, detect_jumpmaster, (sc,))
     currentIteration = currentIteration + 1
     if (max_val >= 0.8):
-        sendDiscordDM("DU BIST JUMPASTER DU NOOB")
+        sendDiscordDM("DU BIST JUMPASTER")
         playRandomSound()
         s.cancel(event)
         detect_champion_selection(sc)
@@ -134,7 +193,7 @@ def detect_jumpmaster(sc):
 def detect_champion_selection(sc):
     max_val = image_comp(isJumpmaster=False)
     print(
-        f"character selection detected: {'true' if max_val >= 0.8 else 'false'}")
+        f"Champion selection detected: {'true' if max_val >= 0.8 else 'false'}")
 
     event = s.enter(20, 1, detect_champion_selection, (sc,))
     if max_val >= 0.8:
