63ad41e5e4c4719eb15839005320668f8bdc0f3a
Lucas <lucaskeller98@gmail.com>
2021-09-25 15:05:24 +0200

    fancy terminal

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
 import Titlebar from "components/titlebar/Titlebar";
@@ -35,7 +35,7 @@ function App() {
     });
 
     s.on("logs", (log) => {
-      setLogs((prevLogs) => [...prevLogs, log].slice(-5));
+      setLogs((prevLogs) => [...prevLogs, log]);
     });
 
     return () => {
@@ -56,44 +56,38 @@ function App() {
     <>
       <Titlebar />
 
-      <div className={styles.app}>
-        <div className={styles["app-header"]}>
-          <div className={`${styles.col} ${styles.leftSide}`}>
-            <div className={styles.headingSection}>
-              <img
-                src={logo}
-                className={`${styles["app-logo"]} ${
-                  socketStatus && styles["app-logo-animation"]
-                } ${status === "On" && styles.red}`}
-                alt="logo"
-              />
-              <p>Apex Jumpmaster Alert</p>
-            </div>
-            <div className={styles.toggleSection}>
-              <hr />
-              <p className={styles.sectionTitle}>General</p>
-              <Toggle
-                label="Toggle Jumpmaster Alert"
-                id="startStop"
-                onClick={startAndStop}
-              />
-              <hr />
-              <p className={styles.sectionTitle}>Settings</p>
-              <Toggle
-                label="Toggle"
-                id="1"
-                onClick={() => {
-                  console.log(1);
-                }}
-              />
-              <Toggle label="Toggle" id="2" onClick={() => {}} />
-              <Toggle label="Toggle" id="3" onClick={() => {}} />
-              <Toggle label="Toggle" id="4" onClick={() => {}} />
-            </div>
+      <div className={styles["app-header"]}>
+        <div className={`${styles.col} ${styles.leftSide}`}>
+          <div className={styles.headingSection}>
+            <img
+              src={logo}
+              className={`${styles["app-logo"]} ${socketStatus && styles["app-logo-animation"]} ${
+                status === "On" && styles.red
+              }`}
+              alt="logo"
+            />
+            <p>Apex Jumpmaster Alert</p>
           </div>
-          <div className={styles.col}>
-            <Terminal logs={logs} />
+          <div className={styles.toggleSection}>
+            <hr />
+            <p className={styles.sectionTitle}>General</p>
+            <Toggle label="Toggle Jumpmaster Alert" id="startStop" onClick={startAndStop} />
+            <hr />
+            <p className={styles.sectionTitle}>Settings</p>
+            <Toggle
+              label="Toggle"
+              id="1"
+              onClick={() => {
+                console.log(1);
+              }}
+            />
+            <Toggle label="Toggle" id="2" onClick={() => {}} />
+            <Toggle label="Toggle" id="3" onClick={() => {}} />
+            <Toggle label="Toggle" id="4" onClick={() => {}} />
           </div>
+        </div>
+        <div className={styles.col}>
+          <Terminal logs={logs} />
         </div>
       </div>
     </>
@@ -102,12 +96,42 @@ function App() {
 
 export default App;
 
-const Terminal = ({ logs }) => {
+const TerminalMessage = ({ message }) => {
   return (
-    <div className={styles.terminal}>
-      {logs.map((log, i) => (
-        <p>{log}</p>
-      ))}
+    <div className={`${styles.instruction}`}>
+      <span className={`${styles.user}`}>apex_alert@log: </span>
+      <span className={`${styles.command}`}>{message}</span>
+    </div>
+  );
+};
+
+const Terminal = ({ logs }) => {
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [logs]);
+
+  return (
+    <div className={`${styles["cv-code"]}`}>
+      <div className={`${styles["cv-code-header"]}`}>
+        <div className={`${styles["cv-code-buttons"]}`}>
+          <span className={`${styles["fake-button"]}`} data-close />
+          <span className={`${styles["fake-button"]}`} data-minify />
+          <span className={`${styles["fake-button"]}`} data-expand />
+        </div>
+        <span className={`${styles["cv-code-title"]}`}>apex_alert@terminal: ~</span>
+      </div>
+      <div className={`${styles.content} ${styles.scrollbar}`}>
+        {logs.map((log, i) => (
+          <TerminalMessage message={log} key={`message-${i}`} />
+        ))}
+        <div ref={messagesEndRef} />
+      </div>
     </div>
   );
 };
@@ -115,12 +139,7 @@ const Terminal = ({ logs }) => {
 const Toggle = ({ label, id, onClick }) => {
   return (
     <div className={styles.toggleButton}>
-      <input
-        className={styles.hide}
-        id={id}
-        type="checkbox"
-        onClick={onClick}
-      />
+      <input className={styles.hide} id={id} type="checkbox" onClick={onClick} />
       <label htmlFor={id} className={styles.toggle} />
       <span className={styles.label}>{label}</span>
     </div>
